57dd0f64b1fc84f6a4595bcc6198e2a9de348167
James Boot <44063127+jamesboot@users.noreply.github.com>
2024-02-12 09:24:15 +0000

    Add files via upload

---
+++ star_align.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Set queue options
+#$ -m e 					                # Email when completed
+#$ -M j.boot@qmul.ac.uk 	        # Send email to this address
+#$ -cwd						                # Set the working directory for the job to current directory
+#$ -pe smp 4				              # Request 4 cores
+#$ -l h_vmem=8G				            # Request 8GB RAM
+#$ -j y						                # Join stdout and stderr - standard output and standard error stream
+#$ -l h_rt=24:00:00			          # Request 10 hrs runtime
+#$ -t 1-58					              # Threading option - this should match the number of samples 
+
+# Do not edit this unless the parameters file name needs changing 
+SAMPLES=$(sed -n "${SGE_TASK_ID}p" Samples.txt)
+FASTQDIR=/data/WHRI-GenomeCentre/shares/Projects/Bioinformatic/Lin_Yung-Yao/BulkRNAseq/Data/SRA_Download
+
+# Load module
+module load star/2.7.0f
+
+# Run STAR with --quantMode GeneCounts so that gene level counts are automatically output - then htseq does not need to be run
+STAR --outSAMstrandField intronMotif \
+--quantMode GeneCounts \
+--outSAMtype BAM SortedByCoordinate \
+--genomeDir /data/WHRI-GenomeCentre/Genome/Human/hg19.GRCh38.104/star \
+--readFilesIn ${FASTQDIR}/${SAMPLES}_1.fastq ${FASTQDIR}/${SAMPLES}_2.fastq \
+--outFileNamePrefix ${SAMPLES}_ \
+--runThreadN ${NSLOTS}	
