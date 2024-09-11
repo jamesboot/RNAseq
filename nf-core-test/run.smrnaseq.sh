307645949d318bf65bc5e0d14216153ebb097777
James Boot <james.boot@crick.ac.uk>
2024-09-11 12:59:26 +0100

    Create test scripts

--- nf-core-test/run.smrnaseq.sh
+++ nf-core-test/run.smrnaseq.sh
@@ -1,1 +1,8 @@
-sbatch -N 1 -c 1 --partition ncpu --time 72:00:00 -J smrnaseq --wrap "sh /nemo/stp/babs/working/mitterr/projects/patanir/hamish.crerar/hc682_deseq2/smrnaseq/smrnaseq.sh"
+#!/bin/bash
+#SBATCH --job-name=nfcore-test
+#SBATCH --nodes=1
+#SBATCH -c=1
+#SBATCH --ntasks=1
+#SBATCH --time=1:00:00
+#SBATCH --partition=ncpu
+#SBATCH --wrap "sh /nemo/stp/babs/working/mitterr/projects/patanir/hamish.crerar/hc682_deseq2/smrnaseq/smrnaseq.sh"
--- nf-core-test/smrnaseq.sh
+++ nf-core-test/smrnaseq.sh
@@ -1,20 +1,13 @@
-## LOAD REQUIRED MODULES
+# LOAD REQUIRED MODULES
 ml purge
-ml Nextflow/22.10.3
-ml Singularity/3.6.4
-ml CAMP_proxy
+ml Nextflow/24.04.2
+ml Singularity/3.11.3
 
-export NXF_HOME=/nemo/stp/babs/working/mitterr/.nextflow/
+export NXF_HOME=/nemo/stp/babs/working/bootj/.nextflow/
 
-## Set NXF_WORK as the absolute NEMO path to the folder containing the pipeline scripts (i.e. not CAMP on a softlink)
-cd /nemo/stp/babs/working/mitterr/projects/patanir/hamish.crerar/hc682_deseq2/smrnaseq
+# Set NXF_WORK as the absolute NEMO path to the folder containing the pipeline scripts (i.e. not CAMP on a softlink)
+cd /nemo/stp/babs/working/bootj/nfcore-test
 export NXF_WORK=$(pwd -P)/work
-
-## Uncomment the code below to set the NXF_WORK in the scratch space.
-#export NXF_WORK=`echo $NXF_WORK | sed 's:^/nemo/stp/babs/working/:/flask/scratch/babs/:'`
-#if [ ! -d "$NXF_WORK" ]; then
-#  ln -s $NXF_WORK .
-#fi
 
 # Singularity is the system we use with nf-core to make software available in a consistent manner.
 # You can leave this line as is - we are using the BABS library of singularity images
@@ -23,22 +16,8 @@ export NXF_SINGULARITY_LIBRARYDIR=/flask/apps/containers/all-singularity-images/
 # You should create a folder in your working area to store any additional images that are not found in the BABS library folder above
 export NXF_SINGULARITY_CACHEDIR=/flask/apps/misc/stp/babs/nf-core/singularity/
 
-## MOTE: For the contaminant files (--rrna, --trna, --cdna, --ncrna, pirna, other_contamination), each FASTA file is first compared to the available miRNA sequences and overlaps are removed.  Thus, no need to pre-filter.
+# TEST PIPELINE
+nextflow run nf-core/rnaseq \
+  --email james.boot@crick.ac.uk \
+  -profile test
 
-## RUN PIPELINE
-nextflow run nf-core/smrnaseq \
-  --input /nemo/stp/babs/working/mitterr/projects/patanir/hamish.crerar/hc682_deseq2/smrnaseq/samplesheet.csv \
-  --protocol custom \
-  --three_prime_adapter AACTGTAGGCACCATCAAT \
-  --genome GRCh38 \
-  --fasta /nemo/stp/babs/working/mitterr/projects/patanir/hamish.crerar/hc682_deseq2/smrnaseq/GRCh38.fa \
-  --mirtrace_species hsa \
-  --mirna_gtf /nemo/stp/babs/working/mitterr/projects/patanir/hamish.crerar/hc682_deseq2/smrnaseq/mirbase/hsa.gff3 \
-  --hairpin /nemo/stp/babs/working/mitterr/projects/patanir/hamish.crerar/hc682_deseq2/smrnaseq/mirbase/hairpin.fa \
-  --mature /nemo/stp/babs/working/mitterr/projects/patanir/hamish.crerar/hc682_deseq2/smrnaseq/mirbase/mature.fa \
-  --email richard.mitter@crick.ac.uk \
-  --outdir results \
-  -profile crick \
-  -c /nemo/stp/babs/working/mitterr/projects/patanir/hamish.crerar/hc682_deseq2/smrnaseq/nf_config.txt \
-  -r 2.2.1 \
-  -resume
