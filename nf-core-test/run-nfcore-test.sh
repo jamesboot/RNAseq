206e4a75b07a37def225edf0cc1da087789f0e1c
James Boot <james.boot@crick.ac.uk>
2024-09-11 13:11:56 +0100

    Correct --wrap

--- nf-core-test/run-nfcore-test.sh
+++ nf-core-test/run-nfcore-test.sh
@@ -5,4 +5,4 @@
 #SBATCH --ntasks=1
 #SBATCH --time=1:00:00
 #SBATCH --partition=ncpu
-#SBATCH --wrap "sh /nemo/stp/babs/working/mitterr/projects/patanir/hamish.crerar/hc682_deseq2/smrnaseq/smrnaseq.sh"
+#SBATCH --wrap "sh /nemo/stp/babs/working/bootj/nfcore-test/nfcore-test.sh"
