40d1471d39be5bc03717ca99a8d5e409b3bb4894
James Boot <james.boot@crick.ac.uk>
2024-09-12 16:13:51 +0100

    Correct nfcore-test.sh

--- nf-core-test/nfcore-test.sh
+++ nf-core-test/nfcore-test.sh
@@ -2,12 +2,7 @@
 ml purge
 ml Nextflow/24.04.2
 ml Singularity/3.11.3
-
-export NXF_HOME=/nemo/stp/babs/working/bootj/.nextflow/
-
-# Set NXF_WORK as the absolute NEMO path to the folder containing the pipeline scripts (i.e. not CAMP on a softlink)
-cd /nemo/stp/babs/working/bootj/nfcore-test
-export NXF_WORK=$(pwd -P)/work
+ml Java/21.0.2
 
 # Singularity is the system we use with nf-core to make software available in a consistent manner.
 # You can leave this line as is - we are using the BABS library of singularity images
@@ -18,6 +13,7 @@ export NXF_SINGULARITY_CACHEDIR=/flask/apps/misc/stp/babs/nf-core/singularity/
 
 # TEST PIPELINE
 nextflow run nf-core/rnaseq \
-  --email james.boot@crick.ac.uk \
-  -profile test,crick
-
+  --outdir outs \
+  -profile test,crick \
+  -r 3.15.0 \
+  -resume
