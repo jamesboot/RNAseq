bc13fe6aa92e4f5ba936c8a4a2b80eaba3c80be7
James Boot <bootj@babs002.nemo.thecrick.org>
2025-03-23 18:49:46 +0000

    Update find_fastqs.sh

--- custom-scripts/find_fastqs.sh
+++ custom-scripts/find_fastqs.sh
@@ -2,19 +2,22 @@
 # Input must be directory to search in
 
 # Find R1 and R2 files 
-find -L $1 -name "*R1_001.fastq.gz" | sort >> r1.txt
-find -L $1 -name "*R2_001.fastq.gz" | sort >> r2.txt
+find -L $1 -name "*R1_001.fastq.gz" >> r1.txt
+find -L $1 -name "*R2_001.fastq.gz" >> r2.txt
+
+sort r1.txt > r1_sort.txt
+sort r2.txt > r2_sort.txt
 
 # Create CSV for results to go in
 echo "sample,fastq_1,fastq_2" >> samplesheet.csv
 
 # In for loop put all into csv
-for i in $(seq 1 $(wc -l < r1.txt));
+for i in $(seq 1 $(wc -l < r1_sort.txt));
 do
     # Get R1
-    R1=$(sed -n "${i}p" r1.txt)
+    R1=$(sed -n "${i}p" r1_sort.txt)
     # Get R2
-    R2=$(sed -n "${i}p" r2.txt)
+    R2=$(sed -n "${i}p" r2_sort.txt)
     # Get basename
     BASE=$(basename "$R1")
     # Get ID
@@ -25,4 +28,6 @@ done
 
 # Clean up
 rm r1.txt
+rm r1_sort.txt
 rm r2.txt
+rm r2_sort.txt
